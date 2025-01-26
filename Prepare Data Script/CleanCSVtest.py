d5ec2ce814b064531e4443a3f160bfb0ab5a844f
ISE <ise.sns46@gmail.com>
2025-01-26 16:05:00 +0700

    Redesign concept ui

--- Prepare Data Script/CleanCSVtest.py
+++ Prepare Data Script/CleanCSVtest.py
@@ -4,4 +4,4 @@ df = pd.read_csv("Prepare Data Script/data.csv", encoding='ISO-8859-1')
 
 df['Revenue'] = (df['Quantity'] * df['UnitPrice']).round(2)
 
-df.to_csv('../ecommerce_data.csv', index=False)
+df.to_csv("Prepare Data Script/ecommerce_data.csv", index=False)
